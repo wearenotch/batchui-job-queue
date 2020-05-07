ebc67110f371bf907eefb377fc10500c9852bc83
Domagoj Madunić <domagoj.madunic@gmail.com>
2020-05-07 23:42:02 +0200

    Added DbQueueAutoConfiguration which enables JpaRepositories from hierarchy

---
+++ src/main/java/com/ag04/batchui/dbqueue/DbQueueAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.ag04.batchui.dbqueue;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories("com.ag04.batchui.dbqueue.repository")
+public class DbQueueAutoConfiguration {
+
+}
